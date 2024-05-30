0130bbfd174a7d474143c4629ca9cc30e36b3c09
DasonTiovino <114289111+DasonTio@users.noreply.github.com>
2024-05-30 12:09:06 +0700

    fix:

--- app/(pages)/place/page.tsx
+++ app/(pages)/place/page.tsx
@@ -48,20 +48,22 @@ const MapComponent = () => {
   };
 
   useEffect(() => {
-    setCustomIcon(
-      new L.Icon({
-        iconUrl:
-          "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-        iconRetinaUrl:
-          "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-        shadowUrl:
-          "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41],
-      })
-    );
+    if (typeof window !== "undefined") {
+      setCustomIcon(
+        new L.Icon({
+          iconUrl:
+            "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+          iconRetinaUrl:
+            "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+          shadowUrl:
+            "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+          iconSize: [25, 41],
+          iconAnchor: [12, 41],
+          popupAnchor: [1, -34],
+          shadowSize: [41, 41],
+        })
+      );
+    }
 
     fetchVotePlaces();
   }, []);
