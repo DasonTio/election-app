a96ac392b12ae9beca374c818443f3273702c1c0
DasonTiovino <114289111+DasonTio@users.noreply.github.com>
2024-05-30 11:41:45 +0700

    fix: window in server side

--- app/(pages)/place/page.tsx
+++ app/(pages)/place/page.tsx
@@ -17,21 +17,6 @@ import ContainerComponent from "@/components/base/containerComponent";
 
 let customIcon: L.Icon;
 
-if (typeof window !== "undefined") {
-  const customIcon = new L.Icon({
-    iconUrl:
-      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-    iconRetinaUrl:
-      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-    shadowUrl:
-      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41],
-  });
-}
-
 enum ModalState {
   edit = "edit",
   add = "add",
